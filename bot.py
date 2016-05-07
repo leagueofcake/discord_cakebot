a21ca6608fb0c7ef1702df0f6a0e8f6d5c754351
Dominic Fung <demonic997@hotmail.com>
2016-05-07 15:48:42 +1000

    !timedcats takes 2 optional parameters now (defaults to 5 m)
    e.g. !timedcats 10 h, !timedcats 3 m

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ invite = 'https://discordapp.com/oauth2/authorize?client_id=178312661233172480&s
 def parse_command_args(command):
     splitted = command.split(' ')
     if len(splitted) >= 2:
-        return splitted[1]
+        return splitted[1:]
     return None
 
 def is_integer(text):
@@ -19,12 +19,13 @@ def is_integer(text):
     except ValueError:
         return False
 
-@client.event
-async def on_server_join(server):
-    channels = client.get_all_channels()
-    for channel in channels:
-        if str(channel.type) == 'text':
-            await client.send_message(channel, 'Hi everyone!')
+# @client.event
+# async def on_server_join(server):
+#     pass
+#     channels = client.get_all_channels()
+#     for channel in channels:
+#         if str(channel.type) == 'text':
+#             await client.send_message(channel, 'Hi everyone!')
 
 @client.event
 async def on_ready():
@@ -32,35 +33,58 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    channels = client.get_all_channels()
-    for channel in channels:
-        if str(channel.type) == 'text':
-            await client.send_message(channel, 'Hi everyone! I\'m online now!')
+    # channels = client.get_all_channels()
+    # for channel in channels:
+    #     if str(channel.type) == 'text':
+    #         await client.send_message(channel, 'Hi everyone! I\'m online now!')
 
 @client.event
 async def on_message(message):
     content = message.content
-
     if content.startswith('!hello'):
         await client.send_message(message.channel, 'Hello {}!'.format(message.author.mention))
     elif content.startswith('!timedcats'):
-        duration = 5
-        args = parse_command_args(content)
-        if is_integer(args):
-            if int(args) <= 60:
-                duration = int(args)
+        if str(message.author) == 'leagueofcake#5979':
+            times = 5
+            duration_str = 'm'
 
-        await client.send_message(message.channel, 'Sending cats every minute for {} minutes!'.format(duration))
-        loop = asyncio.get_event_loop()
-        end_time = loop.time() + 60 * duration
+            args = parse_command_args(content)
 
-        while True:
-            cat_url = requests.get('http://random.cat/meow').json()['file']
-            await client.send_message(message.channel, cat_url)
-            if (loop.time() + 60) >= end_time:
-                break
-            await(asyncio.sleep(60))
-        await client.send_message(message.channel, 'Finished sending cats!')
+            if len(args) > 0:
+                arg_times = args[0]
+                if is_integer(arg_times):
+                    if int(arg_times) <= 60:
+                        times = int(arg_times)
+
+                if len(args) > 1:
+                    arg_duration = args[1]
+                    if arg_duration in cakebot_config.time_map:
+                        duration_str = arg_duration
+
+            unit_time = cakebot_config.time_map[duration_str][0]
+            duration_time = unit_time * times
+
+            unit_duration_str = cakebot_config.time_map[duration_str][1]
+            long_duration_str = cakebot_config.time_map[duration_str][2]
+
+            if times == 1:
+                long_duration_str = unit_duration_str
+
+            sending_msg = 'Sending cats every {} for {} {}!'.format(unit_duration_str, times, long_duration_str)
+            await client.send_message(message.channel, sending_msg)
+
+            loop = asyncio.get_event_loop()
+            end_time = loop.time() + duration_time
+
+            while True:
+                cat_url = requests.get('http://random.cat/meow').json()['file']
+                await client.send_message(message.channel, cat_url)
+                if (loop.time() + unit_time) >= end_time:
+                    break
+                await(asyncio.sleep(unit_time))
+            await client.send_message(message.channel, 'Finished sending cats!')
+        else:
+            await client.send_message(message.channel, 'Only leagueofcake can send cats right now, sorry :(')
     elif content.startswith('!help'):
         tmp = await client.send_message(message.channel, cakebot_config.help_text)
         await(asyncio.sleep(5))
