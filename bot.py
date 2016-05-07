8edaf579154ca40e77dc24a4e59952da99b11536
Dominic Fung <demonic997@hotmail.com>
2016-05-07 12:40:29 +1000

    Create variable for message.content

--- bot.py
+++ bot.py
@@ -14,7 +14,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
+    content = message.content
+    if content.startswith('!test'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
@@ -22,13 +23,13 @@ async def on_message(message):
                 counter += 1
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
+    elif content.startswith('!sleep'):
         await client.send_message(message.channel, 'Sleeping for 5 seconds!')
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping!')
-    elif message.content.startswith('!hello'):
+    elif content.startswith('!hello'):
         await client.send_message(message.channel, 'Hello {}!'.format(message.author.mention))
-    elif message.content.startswith('!'):
+    elif content.startswith('!'):
         await client.send_message(message.channel, 'Unknown command!')
 
 client.run(cakebot_config.token)
