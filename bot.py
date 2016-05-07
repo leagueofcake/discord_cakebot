bdc6e234c1cddaf8622bdb67e82ecb7c4795c175
Dominic Fung <demonic997@hotmail.com>
2016-05-07 14:44:53 +1000

    Says hello when joining a server/comes online

--- bot.py
+++ bot.py
@@ -6,11 +6,22 @@ client = discord.Client()
 invite = 'https://discordapp.com/oauth2/authorize?client_id=178312661233172480&scope=bot&permissions=66186303'
 
 @client.event
+async def on_server_join(server):
+    channels = client.get_all_channels()
+    for channel in channels:
+        if str(channel.type) == 'text':
+            await client.send_message(channel, 'Hi everyone!')
+
+@client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
+    channels = client.get_all_channels()
+    for channel in channels:
+        if str(channel.type) == 'text':
+            await client.send_message(channel, 'Hi everyone! I\'m online now!')
 
 @client.event
 async def on_message(message):
