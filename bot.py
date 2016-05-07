eb46e46aa37a51021362b8cedb63762b8ec7e036
Dominic Fung <demonic997@hotmail.com>
2016-05-07 12:35:38 +1000

    Add commands (!test, !sleep, !hello, check unknown command)

--- bot.py
+++ bot.py
@@ -12,4 +12,23 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await client.send_message(message.channel, 'Sleeping for 5 seconds!')
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping!')
+    elif message.content.startswith('!hello'):
+        await client.send_message(message.channel, 'Hello {}!'.format(message.author.mention))
+    elif message.content.startswith('!'):
+        await client.send_message(message.channel, 'Unknown command!')
+
 client.run(cakebot_config.token)
