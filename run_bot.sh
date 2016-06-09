70008bcd10cbccf3708083cabd0c5e361d7c4de4
leagueofcake <demonic997@hotmail.com>
2016-06-09 15:15:53 +1000

    Un-ignore run_bot.sh, add run_bot.sh

---
+++ run_bot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PYV=`python -c "import sys; print(sys.version_info[:])"`
+PRETTY_PYV=`python -c "import sys; print('.'.join(map(str, sys.version_info[:3])))"`
+
+echo "Checking Python version >= 3.5..."
+echo "Using $PRETTY_PYV"
+
+if [ ${PRETTY_PYV:0:1} = "3" ] && [ ${PRETTY_PYV:2:1} > 5 ]
+then
+   echo "Valid Python version! Starting bot..."
+   python3 bot.py
+else
+   echo "Unsupported version of Python. Please upgrade to at least 3.5."
+fi
