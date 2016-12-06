e4691249142ebe317736d5d0870b177314630f38
leagueofcake <domokun997@gmail.com>
2016-12-06 16:42:35 +1100

    Change run_bot.sh to restart cakebot if it disconnects/crashes

--- run_bot.sh
+++ run_bot.sh
@@ -9,7 +9,11 @@ if [ ${PRETTY_PYV:0:1} -eq "3" ] && [ ${PRETTY_PYV:2:1} -ge "5" ]
 then
    echo "Valid Python version! Starting bot..."
    echo "------"
-   python bot.py
+   until python bot.py
+   do
+      echo "Bot crashed with exit code $?. Restarting..." >&2
+      sleep 5
+   done
 else
    echo "Unsupported version of Python. Please upgrade to at least 3.5."
 fi
